eb544c0991f1f361c754578666e5622f28f28832
Duncan McClean <duncan@steadfastcollective.com>
2021-01-29 16:55:51 +0000

    base of recordings endpoint trait

---
+++ src/Endpoints/Recordings.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Steadfastcollective\LaravelDailyco\Endpoints;
+
+trait Recordings
+{
+    public function recordings()
+    {
+        //
+    }
+
+    public function recording()
+    {
+        //
+    }
+
+    public function deleteRecording()
+    {
+        //
+    }
+
+    public function recordingAccessLink()
+    {
+        //
+    }
+
+    public function recordingDownload()
+    {
+        //
+    }
+
+    public function createRecordingCompositesReceipe()
+    {
+        //
+    }
+
+    public function recordingComposites()
+    {
+        //
+    }
+}
