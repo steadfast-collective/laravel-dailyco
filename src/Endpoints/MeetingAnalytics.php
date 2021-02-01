972c699e3eb4a9c7c519b03850256b08f1963425
Duncan McClean <duncan@steadfastcollective.com>
2021-02-01 08:51:12 +0000

    Rename method

--- src/Endpoints/MeetingAnalytics.php
+++ src/Endpoints/MeetingAnalytics.php
@@ -4,7 +4,7 @@ namespace Steadfastcollective\LaravelDailyco\Endpoints;
 
 class MeetingAnalytics
 {
-    public function meetings(array $data = [])
+    public function meetingAnalytics(array $data = [])
     {
         return $this->get('meetings', $data);
     }
